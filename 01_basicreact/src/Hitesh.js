278bfab626023966acc0f121a7ecbe9f2c204421
Hitesh <choudharyhitesh905@gmail.com>
2025-10-26 16:40:58 +0530

    new Start

--- 01_basicreact/src/Hitesh.js
+++
@@ -1,8 +0,0 @@
-
-function Hitesh(){
-    return(
-        <h3>Hello Dunia !!!</h3>
-    )
-}
-
-export default Hitesh
--- 01_vitereact/src/Hitesh.jsx
+++
@@ -1,8 +0,0 @@
-
-function Hitesh (){
-    return (
-        <h1>Hello Dunia </h1>
-    )
-}
-
-export default Hitesh
