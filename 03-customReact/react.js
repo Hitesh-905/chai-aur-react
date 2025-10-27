d520f6e3a58b5933a5c27d6c36b600f4af320410
Hitesh <choudharyhitesh905@gmail.com>
2025-10-27 20:56:51 +0530

    optimized code foe our custom react

--- 03-customReact/react.js
+++ 03-customReact/react.js
@@ -2,9 +2,13 @@
 function customRender(reactElement,mainContainer){
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
-    domElement.setAttribute('href',reactElement.props.href)
-    domElement.setAttribute('target',reactElement.props.target)
-    mainContainer.appendChild(domElement)
+    // domElement.setAttribute('href',reactElement.props.href)
+    // domElement.setAttribute('target',reactElement.props.target)
+    // mainContainer.appendChild(domElement)
+    for (const prop in reactElement.props) {
+       domElement.setAttribute(prop,reactElement.props[prop])
+    }
+     mainContainer.appendChild(domElement);
 }
 const reactElement = {
     type : 'a',
