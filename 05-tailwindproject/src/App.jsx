2f8fd38c53c47b846ce99e37ca15012ef85f1338
Hitesh <choudharyhitesh905@gmail.com>
2025-10-28 20:52:24 +0530

    tailwind setup through vite and props use case

---
+++ 05-tailwindproject/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Card from './components/Card'
+function App() {
+  return (
+    <>
+    <Card username='Hitesh'
+    btn='click here'/>
+     <Card username='Bhavesh'
+      />
+
+    </>
+    
+  );
+}
+
+
+
+export default App
